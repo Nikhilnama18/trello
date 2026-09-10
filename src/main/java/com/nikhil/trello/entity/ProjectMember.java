5be3a6f05f255096069a58086e706a8f9b7914a6
Nikhil_Nama <nikhilnama1111@gmail.com>
2026-09-10 15:30:41 +0530

    Introduce Project & Project Member entities

---
+++ src/main/java/com/nikhil/trello/entity/ProjectMember.java
@@ -0,0 +1,46 @@
+package com.nikhil.trello.entity;
+
+import com.nikhil.trello.model.ProjectRole;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(
+        name = "project_members",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"project_id" , "user_id"})
+        }
+)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProjectMember {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ProjectRole role;
+
+    @CreationTimestamp
+    private Instant createdAt;
+
+    public ProjectMember(Project project, User user, ProjectRole role){
+        this.project = project;
+        this.user = user;
+        this.role = role;
+    }
+}
---
+++ src/main/java/com/nikhil/trello/model/ProjectRole.java
@@ -0,0 +1,8 @@
+package com.nikhil.trello.model;
+
+public enum ProjectRole {
+    OWNER,
+    MEMBER,
+    COMMENTER,
+    VIEWER
+}
